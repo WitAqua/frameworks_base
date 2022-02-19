079495cf726c7481cdf1c1cbac7002cc59cef123
Pranav Vashi <neobuddy89@gmail.com>
2022-02-19 22:46:09 +0530

    Add support for Navbar styles
    Change-Id: I01b34d2701241cafcf7139af0eada716b030bb6d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6498,6 +6498,12 @@ public final class Settings {
         public static final String STATUSBAR_NOTIF_COUNT = "statusbar_notif_count";
 
         /**
+         * Navbar style
+         * @hide
+         */
+        public static final String NAVBAR_STYLE = "navbar_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
