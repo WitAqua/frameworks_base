bad0e817aa70c41f4cad5ce70e1b77651c625a48
Toufu <hamm157famm7423@gmail.com>
2025-11-28 19:52:05 +0900

    toast: Add scrolling toast texts switch [1/2]
    Change-Id: I9451c448f77f1bc7687438c11a39a9863fda5fc7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6916,6 +6916,12 @@ public final class Settings {
         public static final String ISLAND_NOTIFICATION_NOW_PLAYING = "island_notification_now_playing";
 
         /**
+         * Whether to scroll long texts on toast
+         * @hide
+         */
+        public static final String ENABLE_SCROLL_TOAST_TEXT = "enable_scroll_toast_text";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
