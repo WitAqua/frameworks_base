2by2-Project/android_frameworks_base

monet: Use Style.VIBRANT theme by default

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -162,7 +162,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     private float mContrast = 0;
     // Theme variant: Vibrant, Tonal, Expressive, etc
     @VisibleForTesting
-    protected Style mThemeStyle = Style.TONAL_SPOT;
+    protected Style mThemeStyle = Style.VIBRANT;
     // Accent colors overlay
     private FabricatedOverlay mSecondaryOverlay;
     // Neutral system colors overlay
@@ -893,11 +893,11 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 style = Style.valueOf(
                         object.getString(ThemeOverlayApplier.OVERLAY_CATEGORY_THEME_STYLE));
                 if (!validStyles.contains(style)) {
-                    style = Style.TONAL_SPOT;
+                    style = Style.VIBRANT;
                 }
             } catch (JSONException | IllegalArgumentException e) {
                 Log.i(TAG, "Failed to parse THEME_CUSTOMIZATION_OVERLAY_PACKAGES.", e);
-                style = Style.TONAL_SPOT;
+                style = Style.VIBRANT;
             }
         }
         return style;

WitAqua/frameworks_base

Change-Id: I84ca0fee475659d2ac5ded9c90f89ca67b214b96

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -163,7 +163,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     // Theme variant: Vibrant, Tonal, Expressive, etc
     @VisibleForTesting
     @ThemeStyle.Type
-    protected int mThemeStyle = ThemeStyle.TONAL_SPOT;
+    protected int mThemeStyle = ThemeStyle.VIBRANT;
     // Accent colors overlay
     private FabricatedOverlay mAccentOverlay;
     // Neutral system colors overlay
@@ -913,11 +913,11 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 style = ThemeStyle.valueOf(
                         object.getString(OVERLAY_CATEGORY_THEME_STYLE));
                 if (!validStyles.contains(style)) {
-                    style = ThemeStyle.TONAL_SPOT;
+                    style = ThemeStyle.VIBRANT;
                 }
             } catch (JSONException | IllegalArgumentException e) {
                 Log.i(TAG, "Failed to parse THEME_CUSTOMIZATION_OVERLAY_PACKAGES.", e);
-                style = ThemeStyle.TONAL_SPOT;
+                style = ThemeStyle.VIBRANT;
             }
         }
         return style;